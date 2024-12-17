495ece8766fa4033f118747836f1e3f618f2d184
unknown <sakthiselvamani2005@gmail.com>
2024-12-17 12:42:26 +0530

    Updates

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const latInput = document.getElementById('lat')
 const lonInput = document.getElementById('lon')
 const submit = document.getElementById('submit')
 const key = 'af471dfcaa914e2b9dd71913241612'
+const loc = document.getElementById('location')
 const condition = document.getElementById('condition')
 const temp = document.getElementById('temp')
 const wind = document.getElementById('wind')
@@ -14,6 +15,7 @@ const getData = async () => {
     fetch(req).then(result => result.json())
     .then(result => {
         console.log(result);
+        loc.innerHTML = result.location.name;
         condition.innerHTML = result.current.condition.text;
         temp.innerHTML = `${result.current.temp_c} °C`;
         wind.innerHTML = `${result.current.wind_kph} km/hr`;
